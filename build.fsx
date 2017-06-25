a5f7fa2c2ad65056f0309889cb18c06bbc0209e6
mrange <marten_range@hotmail.com>
2017-06-25 21:52:40 +0200

    More F# idiomatic

--- build.fsx
+++ build.fsx
@@ -1,9 +1,4 @@
-//////////////////////////////////////////////////////////////////////
-// DEPENDENCIES
-//////////////////////////////////////////////////////////////////////
-#r "tools/Cake.Core.0.20.0/lib/net45/Cake.Core.dll"
-#r "tools/Cake.Common.0.20.0/lib/net45/Cake.Common.dll"
-#r "tools/Cake.Bridge.0.0.1-alpha/lib/net45/Cake.Bridge.dll"
+#load "cake.fsx"
 
 //////////////////////////////////////////////////////////////////////
 // NAMESPACE IMPORTS
@@ -17,19 +12,10 @@ open Cake.Common.Tools.DotNetCore.Pack
 open Cake.Core
 open Cake.Core.IO
 open System
-open System.Linq
-//open CakeBridge
 
-// TODO:
-//  1. Improve style
-//  2. Remove the need to specify CakeBridge everywhere
-//  3. More idiomatic handling of global state
+open CakeAdapter.CakeModule
 
 // Execute script with: fsi build.fsx
-
-// "Magic" to handle implicit conversion (which F# isn't really supporting)
-let inline (!>) (x:^a) : ^b = ((^a or ^b) : (static member op_Implicit : ^a -> ^b) x)
-let context                 = CakeBridge.Context
 
 //////////////////////////////////////////////////////////////////////
 // ARGUMENTS
@@ -40,68 +26,97 @@ let configuration = context.Argument("configuration", "Release")
 //////////////////////////////////////////////////////////////////////
 // GLOBALS
 //////////////////////////////////////////////////////////////////////
-let directoryPath           = !> context.Directory("./nuget")
-let nugetRoot               = context.MakeAbsolute(directoryPath)
-let mutable solution        = null : FilePath
-let mutable semVersion      = null : string
-let mutable assemblyVersion = null : string
-let mutable fileVersion     = null : string
+let directoryPath           = !> (context.Directory "./nuget")
+let nugetRoot               = context.MakeAbsolute directoryPath
+
+type ProjectInfo =
+  {
+    AssemblyVersion : string
+    FileVersion     : string
+    SemVersion      : string
+    Solution        : FilePath
+  }
+
+let mutable projectInfo     = None
 
 let argumentCustomizer      = Func<ProcessArgumentBuilder,ProcessArgumentBuilder> (fun args ->
+                                let p = projectInfo.Value
                                 args
-                                  .Append("/p:Version={0}"        , semVersion      )
-                                  .Append("/p:AssemblyVersion={0}", assemblyVersion )
-                                  .Append("/p:FileVersion={0}"    , fileVersion     ))
+                                  .Append("/p:Version={0}"        , p.SemVersion      )
+                                  .Append("/p:AssemblyVersion={0}", p.AssemblyVersion )
+                                  .Append("/p:FileVersion={0}"    , p.FileVersion     ))
 
 //////////////////////////////////////////////////////////////////////
 // SETUP / TEARDOWN
 //////////////////////////////////////////////////////////////////////
 
-CakeBridge.Setup(fun context ->
-    context.Information("Setting up...")
+setup (fun context ->
+    context.Information "Setting up..."
 
-    solution <- context.GetFiles("./src/*.sln").FirstOrDefault()
-    if solution = null then failwith "Failed to find solution"
+    let solution        =
+      match context.GetFiles "./src/*.sln" |> Seq.tryHead with
+      | Some solution -> solution
+      | None          -> failwith "Failed to find solution"
 
-    let releaseNotes  =   context.ParseReleaseNotes(!> "./ReleaseNotes.md")
-    assemblyVersion   <-  releaseNotes.Version.ToString()
-    fileVersion       <-  assemblyVersion
-    semVersion        <-  assemblyVersion + "-alpha"
+    let releaseNotes    = context.ParseReleaseNotes(!> "./ReleaseNotes.md")
+    let assemblyVersion = string releaseNotes.Version
+    let fileVersion     = assemblyVersion
+    let semVersion      = assemblyVersion + "-alpha"
+
+    projectInfo <- Some {
+        AssemblyVersion = assemblyVersion
+        FileVersion     = fileVersion
+        SemVersion      = semVersion
+        Solution        = solution
+      }
 
     context.Information("Executing build {0}...", semVersion)
   )
 
-CakeBridge.Teardown(fun context ->
-    context.Information("Tearing down...")
+tearDown (fun context ->
+    context.Information "Tearing down..."
   )
-
 //////////////////////////////////////////////////////////////////////
 // TASKS
 //////////////////////////////////////////////////////////////////////
 
-let clean = CakeBridge.Task("Clean").Does(fun () ->
+let clean =
+  task "Clean"
+  |> does (fun () ->
     context.CleanDirectories("./src/**/bin/" + configuration)
     context.CleanDirectories("./src/**/obj/" + configuration)
-    context.CleanDirectory(nugetRoot)
+    context.CleanDirectory nugetRoot
   )
 
-let restore = CakeBridge.Task("Restore").IsDependentOn(clean).Does(fun () ->
-    context.DotNetCoreRestore(solution.FullPath)
+let restore =
+  task "Restore"
+  |> isDependentOn clean
+  |> does (fun () ->
+    context.DotNetCoreRestore projectInfo.Value.Solution.FullPath
   )
 
-let build = CakeBridge.Task("Build").IsDependentOn(restore).Does(fun () ->
+let build =
+  task "Build"
+  |> isDependentOn restore
+  |> does (fun () ->
     context.DotNetCoreBuild(
-      solution.FullPath                           ,
+      projectInfo.Value.Solution.FullPath         ,
       DotNetCoreBuildSettings(
         Configuration         = configuration     ,
         ArgumentCustomization = argumentCustomizer))
   )
 
-let pack = CakeBridge.Task("Pack").IsDependentOn(build).Does(fun () ->
-    if context.DirectoryExists(nugetRoot) |> not then
-        context.CreateDirectory(nugetRoot)
+let pack =
+  task "Pack"
+  |> isDependentOn build
+  |> does (fun () ->
+    if context.DirectoryExists nugetRoot |> not then context.CreateDirectory nugetRoot
 
-    for project in context.GetFiles("./src/**/*.csproj").Where(fun file -> file.FullPath.EndsWith("Tests") |> not) do
+    let projectFiles =
+      context.GetFiles "./src/**/*.csproj"
+      |> Seq.filter (fun file -> file.FullPath.EndsWith "Tests" |> not)
+
+    for project in projectFiles do
         context.DotNetCorePack(
           project.FullPath                            ,
           DotNetCorePackSettings(
@@ -111,9 +126,10 @@ let pack = CakeBridge.Task("Pack").IsDependentOn(build).Does(fun () ->
             ArgumentCustomization = argumentCustomizer))
   )
 
-CakeBridge.Task("Default").IsDependentOn(pack)
+task "Default"
+  |> isDependentOn pack
 
 //////////////////////////////////////////////////////////////////////
 // EXECUTION
 //////////////////////////////////////////////////////////////////////
-CakeBridge.RunTarget(target)
+runTarget target
